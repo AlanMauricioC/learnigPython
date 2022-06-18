9e9319947e9dda4cb64294fb0d9fc99ae8fb6753
Alan Mauricio <alanmauricioc@ouitlook.com>
2022-06-18 00:07:42 -0500

    add money conversor

---
+++ moneyconversor.py
@@ -0,0 +1,7 @@
+
+mxn_pesos=float(input('How many Mexican pesos do you have? '))
+dolar_value=20.34
+
+dolars=mxn_pesos/dolar_value
+dolars=round(dolars,2)
+print('you have $'+str(dolars)+' dolars')
