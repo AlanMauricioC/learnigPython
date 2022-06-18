dc7c4fdf3e108eb3501b1d40f693526dfd1759a8
Alan Mauricio <alanmauricioc@ouitlook.com>
2022-06-18 00:32:07 -0500

    add conditionals example

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+age = int(input('How old are you? '))
+
+if age>18 and age<=80:
+  print('You are old enough')
+elif age>80:
+  print('0_0 keep calm and take some rest')
+else:
+  print('You are underage')
