0cd0ecfa8427ff0822a2ba8cbf2e4765615883fe
Alan Mauricio <alanmauricioc@ouitlook.com>
2022-06-18 22:36:30 -0500

    add input menu

--- moneyconversor.py
+++ moneyconversor.py
@@ -1,3 +1,13 @@
+menu="""
+Welcome to currency converter 💰
+
+1 - Colombian Pesos
+2 - Argentine Pesos
+3 - Mexican Pesos
+
+Select an option: """
+
+option=input(menu)
 
 mxn_pesos=float(input('How many Mexican pesos do you have? '))
 dolar_value=20.34
