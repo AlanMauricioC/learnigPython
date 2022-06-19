04e465da323166fffed39847f3c702f96a8b40ef
Alan Mauricio <alanmauricioc@ouitlook.com>
2022-06-18 22:55:33 -0500

    implement funcitons

--- currencyconversor.py
+++ currencyconversor.py
@@ -1,3 +1,10 @@
+def convert_currency(dolar_value,currency):
+  mxn_pesos=float(input('How many '+currency+ ' pesos do you have? '))
+  dolars=mxn_pesos/dolar_value
+  dolars=round(dolars,2)
+  print('you have $'+str(dolars)+' dolars')
+  return dolars
+
 menu="""
 Welcome to currency converter 💰
 
@@ -9,9 +16,12 @@ Select an option: """
 
 option=input(menu)
 
-mxn_pesos=float(input('How many Mexican pesos do you have? '))
-dolar_value=20.34
+if option == '1':
+  convert_currency(3875,'colombian')
+elif option == '2':
+  convert_currency(65,'argeninian')
+elif option == '3':
+  convert_currency(20,'mexican')
+else:
+  print('please select a valid option')
 
-dolars=mxn_pesos/dolar_value
-dolars=round(dolars,2)
-print('you have $'+str(dolars)+' dolars')
