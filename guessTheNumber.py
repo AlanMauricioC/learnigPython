c2318301b19f370e3cca71b238bd3a21230b651e
Alan Mauricio <alanmauricioc@ouitlook.com>
2022-07-11 22:24:44 -0500

    update name

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -4,7 +4,6 @@ def run():
  random_num=random.randint(1,100); 
  selected_num=int(input('choose a number between 1 and 100'))
  while selected_num!=random_num:
-   print(random_num)
    if selected_num>random_num:
      print('Too high :c')
    else:
@@ -12,5 +11,5 @@ def run():
    selected_num=int(input('Choose another number'))
  print('You win!')
 
-if __name__ == '__main__':
+if __name__== '__main__':
   run()
