3c7611e574e5ccf7343e1d62d2eaec3091480e60
Alan Mauricio <alanmauricioc@ouitlook.com>
2022-08-21 23:54:41 -0500

    exercise password generator

---
+++ password_generator.py
@@ -0,0 +1,25 @@
+import random
+
+def generate_password():
+    MAYUS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    MINUS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+
+    characters=MAYUS+MINUS+NUMS+CHARS;
+    password=[]
+
+    password_length=15
+    for _ in range(password_length):
+        random_character=random.choice(characters)
+        password.append(random_character)
+
+    password="".join(password)
+    return password
+
+def run():
+    password=generate_password()
+    print('your new password is: '+ password)
+
+if __name__=='__main__':
+    run()
